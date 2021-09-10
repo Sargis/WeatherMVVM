0092ce81dd168a409bf2137738d2c1883d490a03
Sargis <sargiskhachatryan123@gmail.com>
2021-09-10 20:39:34 +0400

    Initial Commit

---
+++ WeatherMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherMVVM
+//
+//  Created by Sargis Khachatryan on 10.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
